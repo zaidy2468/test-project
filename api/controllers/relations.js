8d4007482927ffced658f6eb7af45662e69bed7e
zaid yousaf <zaidyousaf98@gmail.com>
2023-01-13 19:30:07 +0500

    backend

--- api/controllers/relations.js
+++ api/controllers/relations.js
@@ -1,169 +1,218 @@
-const mongoose=require('mongoose');
-const Categories=require('../models/categories');
-const { db } = require('../models/products');
-const Product_1=require('../models/products');
-const subCategories=require('../models/subcategories');
-const category_product_schema=require('../models/categories&products');
+const Categories = require("../models/categories");
+const Product_1 = require("../models/products");
+const subCategories = require("../models/subcategories");
+const product = require("./products");
 
-exports.set_relations=(req,res,next)=>{
-    const products=new Product_1({
-        name:req.body.name,
-        price:req.body.price,
-        quantity:req.body.quantity
-    })
-    products.save()
-    .then(result=>{
-        console.log("Product created",result);
-        Categories.findByIdAndUpdate(req.params.categoryId,{$push:{products:result._id}})
-        .then(result=>{
-            subCategories.findByIdAndUpdate(req.params.subcategoryId,{$push:{products:result._id}})
+exports.set_relations = (req, res, next) => {
+  const products = new Product_1({
+    name: req.body.name,
+    price: req.body.price,
+    quantity: req.body.quantity,
+  });
+  products
+    .save()
+    .then((result) => {
+      console.log("Product created", result);
+      Categories.findByIdAndUpdate(req.params.categoryId, {
+        $push: { products: result._id },
+      })
+        .then((result) => {
+          subCategories
+            .findByIdAndUpdate(req.params.subcategoryId, {
+              $push: { products: result._id },
+            })
+            .exec()
+            .then((resut) => {
+              console.log(resut);
+              res.status(200).json({
+                message: "subcategory updated",
+                resut: resut,
+              });
+
+              Categories.findById(req.params.categoryId)
+                .populate("products")
                 .exec()
-                .then(resut=>{
-                    console.log(resut);
-                    res.status(200).json({
-                        message:"subcategory updated",
-                        resut:resut
-                    });
-                    
-                    Categories.findById(req.params.categoryId).populate('products')
-                    .exec()
-                    .then(take=>{
-                      console.log(take);
-            
-                    })
-                    .catch(err=>{
-                     console.log(err);
-                        });
+                .then((take) => {
+                  console.log(take);
                 })
-                .catch(err=>{
-                    res.status(500).json({
-                        error:err
-                    })
+                .catch((err) => {
+                  console.log(err);
                 });
-            
-        })
-
-        .catch(errr=>{
-            res.status(500).json({
-                error:errr
+            })
+            .catch((err) => {
+              res.status(500).json({
+                error: err,
+              });
             });
-        });
-        
+        })
 
-    })
-    .catch(err=>{
-        res.status(500).json({
-            error:err
+        .catch((errr) => {
+          res.status(500).json({
+            error: errr,
+          });
         });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
     });
-   
 };
-exports.get_products_by_categoryId=(req,res,next)=>{
-   const _id=req.params.categoryId;
-   
-   Categories.findById(_id).populate('products').populate({path:'subcategory',populate:{path:'products'}})
-   .exec()
-   .then(result=>{
-    console.log(result);
-    res.status(200).json({
-        message:"all products for  the given categoryId",
-        products:result
-        
-    });
-   })
-   .catch(err=>{
-     res.status(500).json({
-        error:err
-     });
-   });
-};
-exports.get_products_subcategoryId=(req,res,next)=>{
-    const _id=req.params.subcategoryId;
-    subCategories.findById({_id:_id})
-    .populate('products')
-    
-    
+exports.get_products_by_categoryId = async (req, res, next) => {
+  const _id = req.params.categoryId;
+
+  await Categories.findById(_id)
+    .populate("products")
+    .populate({ path: "subcategory", populate: { path: "products" } })
     .exec()
-    .then(result=>{
-        console.log(result);
-        res.status(200).json({
-            message:"get_products_by_subcategoryId",
-            products:result
-        });
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({
+        message: "all products for  the given categoryId",
+        products: result,
+      });
     })
-    .catch(err=>{
-        res.status(500).json({
-            error:err
-        });
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
     });
 };
-exports.get_aggregation=(req,res,next)=>{
+exports.get_products_subcategoryId = (req, res, next) => {
+  const _id = req.params.subcategoryId;
+  subCategories
+    .findById({ _id: _id })
+    .populate("products")
 
-
-
-    const categoryId=req.params.categoryId;
-    Categories.aggregate([
-
-        // { $addFields: {
-        //         "productId":{ "$toString": "$_id" }
-        //     }},
-
-
-        {$lookup:{
-            from:"category_product_schemas",
-            // localField: "productId"
-            // foreignField: "product_id"
-            //let:{category_id:Categories._id},
-            "pipeline":[
-                {$match:{$expr:{$eq:[categoryId,"category_product_schemas.category_id"]}}},
-                {$lookup:{
-                    from:"categories",
-                   // let:{product_id:"category_product_schemas.product_id"},
-                    "pipeline":[{$match:{$expr:{$eq:["category_product_schemas.product_id","product1._id"]}},
-                    as:"anything1"
-                }]
-                }},
-                { $unwind: "anything1"},
-                //{ $project : { name:'anything1.name' }}
-            ],
-            as:"anything"
-        }}
-    ])
-    .then(result=>{
-        res.status(200).json({
-            output:result
-        })
+    .exec()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({
+        message: "get_products_by_subcategoryId",
+        products: result,
+      });
     })
-    .catch(err=>[
-        res.status(500).json({
-            error:err
-        })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
+};
+exports.get_Products_By_Category = async (req, res, next) => {
+  try {
+    const { price, category, limit, offset } = req.query;
+    const max_price = +price.lte;
+    const min_price = +price.gte;
+    const _limit = +limit;
+    const _offset = +offset;
+    if (category.length > 1) {
+      await manytomany_ProductsByCategories(
+        req,
+        res,
+        max_price,
+        min_price,
+        _limit,
+        _offset,
+        category
+      );
+    } else if (category.includes("")) {
+      await product.products_get_all(req, res, next);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+manytomany_ProductsByCategories = async (
+  req,
+  res,
+  max_price,
+  min_price,
+  _limit,
+  _offset,
+  category
+) => {
+  try {
+    const response = await Product_1.aggregate([
+      {
+        $addFields: {
+          p_id: { $toString: "$_id" },
+        },
+      },
+      {
+        $lookup: {
+          from: "category_product_schemas",
+          localField: "p_id",
+          foreignField: "product_id",
+          as: "result",
+        },
+      },
+      {
+        $unwind: {
+          path: "$result",
+        },
+      },
+      {
+        $addFields: { c_id: { $toObjectId: "$result.category_id" } },
+      },
+      {
+        $lookup: {
+          from: "categories",
+          localField: "c_id",
+          foreignField: "_id",
+          as: "results",
+        },
+      },
+      {
+        $unwind: {
+          path: "$results",
+        },
+      },
+
+      {
+        $project: {
+          name: 1,
+          price: 1,
+          results: 1,
+          productImage: 1,
+        },
+      },
+      {
+        $addFields: {
+          desc: "$results.description",
+          cat_id: { $toString: "$results._id" },
+        },
+      },
+      {
+        $match: {
+          cat_id: { $in: category },
+        },
+      },
+      {
+        $project: {
+          name: 1,
+          price: 1,
+          desc: 1,
+          productImage: 1,
+        },
+      },
+      {
+        $match: {
+          price: { $gte: min_price, $lte: max_price },
+        },
+      },
+      {
+        $skip: _offset,
+      },
+      {
+        $limit: _limit,
+      },
     ]);
-
-
- 
-}
-/*Categories.aggregate([ {$lookup:{  
-    from:"category_product_schemas", 
-    localField:"categories._id", 
-    foreignField:"category_product_schemas.category_id",
-    "pipeline":[
-        {$match:
-            {category_id:categoryId}
-        }
-    ], 
-    as:"anything"}} ])
-.then(result=>{
-    console.log(result)
     res.status(200).json({
-        output:result
-    })
-
-})
-.catch(err=>{
-    res.status(500).json({
-        error:err
-    })
-})
-
-*/
+      message: "category based products",
+      products: response,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
